42693bdbaf1bf923d18e65b44d3e49b6560fe01e
Victor Carlos <victorcarloss290@gmail.com>
2026-09-19 18:12:11 -0300

    Add files via upload

---
+++ aula1_tipos.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite um valor:"))
+num2 = int(input("Digite um valor:"))
+s = num1 + num2
+
+#print('A soma entre', num1, 'e', num2, 'é igual a',s)
+print(f'A soma entre {num1} e {num2} é igual a {s}')
---
+++ aula2_is.py
@@ -0,0 +1,4 @@
+n1 = (input('Digite um numero:'))
+#print(n1.isalpha())
+print(n1.isnumeric())
+#CONTINUAR - DESAFIO 004
